48e8a7509e4bfa652b76c92b0982f1ff459c7dfa
Joab Owala <owalajoab@gmail.com>
2023-05-11 10:35:56 +0300

    birthday reminder app

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
+
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  
+  const [people, setPeople] = useState(data)
+  
+ return (
+  <main className='main'>
+  <section className='container'>
+    <h3>{people.length} birthdays today</h3>
+    <List people={people}/>
+    <button className='button'onClick={() => setPeople([])}>Clear List</button>
+  </section>
+  </main>
+ 
+  
+  )
 }
 
 export default App;
